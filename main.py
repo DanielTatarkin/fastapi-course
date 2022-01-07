d45f146e0037cfd4084bbf724f27f541b790a96b
Daniel Tatarkin <danieltatarkin@gmail.com>
2022-01-07 00:14:19 -0500

    FastAPI course - added Post model class, basic POST post, GET posts and GET post methods

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+MY_POSTS = [{"title": "Title of post 1", "content": "content of post 1", "id": 1}, {"title": "Title of post 2", "content": "content of post 2", "id": 2}]
+
 
 @app.get("/")
 def root():
     return {"message": "Welcome to my API"}
 
+
 @app.get("/posts")
 def get_posts():
     return {
-        "Post 1": 1,
-        "Post 2": 2
+        "data": MY_POSTS
     }
 
-@app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title {payload['title']} content: {payload['content']}"}
+
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000)
+    MY_POSTS.append(post_dict)
+    return {"post": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    for post in MY_POSTS:
+        if post['id'] == id:
+            return post
+    
+    return {
+        "message": f"Could not find post with id: {id}"
+    }
